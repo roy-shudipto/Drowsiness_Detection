30012136d92a22365ae84bf300de885966546801
roy-shudipto <shudipto67@gmail.com>
2019-02-25 04:40:46 -0600

    Final Project

---
+++ cpp/build/CMakeFiles/drowsinessDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drowsinessDetect.dir/drowsinessDetect.cpp.o"
+  "drowsinessDetect.pdb"
+  "drowsinessDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drowsinessDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
